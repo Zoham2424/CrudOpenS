cde814a39e1ceffacf0719318b44cbc1b8778f2a
Ian A <iaalvaradocubero@email.neit.edu>
2025-02-19 18:31:07 -0500

    aa

--- app.js
+++ app.js
@@ -63,6 +63,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB Connection Failed:", err));
 const db = mongoose.connection;
 //check for connection
 db.on("error", console.error.bind(console, "MonoDB Connection error"));
