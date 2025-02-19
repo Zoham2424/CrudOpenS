376f7072359ee0de3b298d818798d17a4071eb76
Ian A <iaalvaradocubero@email.neit.edu>
2025-02-19 18:29:26 -0500

    fix

--- app.js
+++ app.js
@@ -59,7 +59,10 @@ app.use("/", require("./routes/crud"));
 //MongoDB Database connection
 // const mongoURI = "mongodb://localhost:27017/gamelibrary"
 const mongoURI = process.env.MONGO_URI; //||  "mongodb://localhost:27017/gamelibrary"
-mongoose.connect(mongoURI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 const db = mongoose.connection;
 //check for connection
 db.on("error", console.error.bind(console, "MonoDB Connection error"));
